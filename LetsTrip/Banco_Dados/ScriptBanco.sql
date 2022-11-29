39125cc656644a67cba78f0a8158b881c2d3283f
MaessioSptech <maessio.sousa@sptech.school>
2022-11-29 18:03:11 -0300

    Nome das colunas no banco

--- LetsTrip/Banco_Dados/ScriptBanco.sql
+++ LetsTrip/Banco_Dados/ScriptBanco.sql
@@ -16,13 +16,11 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE comentarioSite (
-   id INT,
-   voto INT,
+   id INT auto_increment,
    comentario VARCHAR(150),
    fkUsuario INT,
    FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
-   PRIMARY KEY (id, fkUsuario),
-   CONSTRAINT chkVoto CHECK (voto = 0 OR voto = 1 OR voto = 2 OR voto = 3 OR voto = 4 OR voto = 5)
+   PRIMARY KEY (id, fkUsuario)
 );
 
 INSERT INTO viagem (nome_viagem) VALUES
@@ -32,10 +30,11 @@ INSERT INTO viagem (nome_viagem) VALUES
     ('Joanópolis'),
     ('Campos do Jordão'),
     ('Águas de São Pedro'),
-    ('Parati'),
+    ('Paraty'),
     ('Extrema'),
-    ('Águas de Lindoia'),
+    ('Águas de Lindóia'),
     ('Serra Negra');
+    
 
 INSERT INTO usuario (nome, email, senha) VALUES
 	('Maessio Sousa', 'maessiosousa@sptech.com', '12345678'),
@@ -56,7 +55,6 @@ INSERT INTO usuario (nome, email, senha) VALUES
     ('Suysi', 'suysi@sptech.com', '12345678'),
     ('Wuoga', 'wuoga@sptech.com', '12345678'),
     ('Cyeme', 'Cyeme@sptech.com', '12345678'),
-	('Arcoa', 'Arcoa@sptech.com', '12345678'),
     ('Ciako', 'Ciako@sptech.com', '12345678'),
     ('Xiucu', 'Xiucu@sptech.com', '12345678'),
     ('Gahoil', 'Gahoil@sptech.com', '12345678'),
@@ -64,27 +62,29 @@ INSERT INTO usuario (nome, email, senha) VALUES
     ('Hemoy', 'Hemoy@sptech.com', '12345678'),
     ('Keohu', 'Keohu@sptech.com', '12345678'),
     ('Fuvyo', 'fuvyo@sptech.com', '12345678');
-    
 
-
-INSERT INTO comentarioSite VALUES 
-	  (1 , 5, 'Site incrível', 1),
-      (2 , 4, 'n voluptate velit esse cillum ', 2),
-      (3 , 3, 'at nulla pariat', 3),
-      (4 , 2, 'Excepteur sint occaec', 4),
-      (5 , 1, 'dolor sit amet, ', 5),
-      (6 , 5, ' do eiusmod tempor incidid', 6),
-      (7 , 4, 'lore magna aliqua. Ut e', 7),
-      (8 , 3, 'sed do eiusmod tempor i', 8),
-      (9 , 2, 'magni dolores eos qui r', 9),
-      (10 , 1, 'trum exercitatione', 10),
-      (11 , 5, 'nisi ut aliquid ex ea commodi conseq', 11),
-      (12 , 4, 'iure reprehenderit qui in ', 12),
-      (13 , 3, 'inventore veritatis et qu', 13),
-      (14 , 2, 'sed quia consequuntur m', 14),
-      (15 , 1, 'laboriosam, nisi ut a', 15);
-
+INSERT INTO comentarioSite (comentario, fkUsuario) VALUES 
+	  ('Site incrível', 1),
+      ('n voluptate velit esse cillum ', 2),
+      ('at nulla pariat', 3),
+      ('Excepteur sint occaec', 4),
+      ('dolor sit amet, ', 5),
+      (' do eiusmod tempor incidid', 6),
+      ('lore magna aliqua. Ut e', 7),
+      ('sed do eiusmod tempor i', 8),
+      ('magni dolores eos qui r', 9),
+      ('trum exercitatione', 10),
+      ('nisi ut aliquid ex ea commodi conseq', 11),
+      ('iure reprehenderit qui in ', 12),
+      ('inventore veritatis et qu', 13),
+      ('sed quia consequuntur m', 14),
+      ('laboriosam, nisi ut a', 15);
 
 SELECT * FROM usuario;
 SELECT * FROM comentarioSite;
-SELECT * FROM viagem;
+SELECT * FROM viagem order by nome_viagem;
+
+SELECT v.nome_viagem, count(u.fkViagem) AS qtd_viagem
+                FROM usuario u JOIN viagem v ON u.fkViagem = v.id 
+                    WHERE u.fkViagem IS NOT NULL 
+                        GROUP BY u.fkViagem ORDER BY v.nome_viagem;
